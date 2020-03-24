990119862d70b69bffd4c5ed9b01d9c8de9c1d65
Yutian Zhao <yutianzhao123@gmail.com>
2020-03-24 11:27:22 -0400

    Initial User Model

---
+++ ProductService.Client/src/app/Models/Users/Users.Model.ts
@@ -0,0 +1,17 @@
+export class Users {
+    UserId: number;
+    FirstName: string;
+    MiddleName: string;
+    LastName: string;
+    Email: string;
+    PhoneNumber: string;
+    Address1: string;
+    Address2?: string;
+    City: string;
+    State?: string;
+    Country: string;
+    Zip: string;
+    UserName: string;
+    CreateDate: Date;
+    UpdateDate?: Date;
+}
