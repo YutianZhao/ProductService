0252f1cf773c0cd9949949ffe0b391b002e896be
Yutian Zhao <yutianzhao123@gmail.com>
2020-03-25 13:21:26 -0400

    Added Angular Material

--- ProductService.Client/src/app/app.module.ts
+++ ProductService.Client/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { UsersComponent } from './Components/users/users.component';
 import { ProductsComponent } from './Components/products/products.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { ProductsComponent } from './Components/products/products.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
